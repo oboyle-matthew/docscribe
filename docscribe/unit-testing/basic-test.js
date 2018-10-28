0a1709693dc545d3e98c1307ee013a2bb9880752
Nikolas Bramblett <nlb27@duke.edu>
2018-10-28 14:33:01 -0400

    Test builds snapshot, but the snapshot is null #61

--- docscribe/unit-testing/basic-test.js
+++ docscribe/unit-testing/basic-test.js
@@ -1,6 +1,10 @@
 import 'react-native';
 import React from 'react';
-
 import renderer from 'react-test-renderer';
+import Pain from '../src/Pain';
 
-it('renders correctly', () => {});
+it('renders correctly', () => {
+  const nav = { navigate: () => {} };
+  const pain = renderer.create(<Pain navigation={nav} />).toJSON();
+  expect(pain).toMatchSnapshot();
+});

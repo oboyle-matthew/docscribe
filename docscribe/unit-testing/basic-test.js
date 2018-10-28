432752549c70f8dc058a1e860bf2f3196fbf0806
Nikolas Bramblett <nlb27@duke.edu>
2018-10-28 14:45:39 -0400

    Commented test for future reference #61

--- docscribe/unit-testing/basic-test.js
+++ docscribe/unit-testing/basic-test.js
@@ -4,6 +4,8 @@ import renderer from 'react-test-renderer';
 import BinaryQuestion from '../src/components/BinaryQuestion';
 
 // Basic test made! (Try changing option two to "maybe" and it will fail!)
+// Snapshots basically take a "picture" of your code (serialized I think) and then in future tests
+// You can compare the component to the original snapshot. "npm test -- -u" will overwrite the snapshot for a test.
 it('renders correctly', () => {
   const pain = renderer
     .create(

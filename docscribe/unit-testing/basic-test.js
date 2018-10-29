2053b22fd536bca84d4e9efc7b7fe5366ec437ad
Nikolas Bramblett <nlb27@duke.edu>
2018-10-29 00:24:36 -0400

    Added test case for SliderQuestion #61

--- docscribe/unit-testing/basic-test.js
+++ docscribe/unit-testing/basic-test.js
@@ -1,8 +1,9 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { CheckBox } from 'react-native-elements';
+import { CheckBox, Slider } from 'react-native-elements';
 import BinaryQuestion from '../src/components/BinaryQuestion';
+import SliderQuestion from '../src/components/SliderQuestion';
 
 // Basic test made! (Try changing option two to "maybe" and it will fail!)
 // Snapshots basically take a "picture" of your code (serialized I think) and then in future tests
@@ -42,5 +43,25 @@ describe('<BinaryQuestion/>', () => {
       box.props.onPress(); // apparently onpress is both a prop and a method so you can just...call it.
       expect(inst.root.instance.state.oneChecked).not.toBe(one);
     }
+  });
+});
+
+describe('<SliderQuestion/>', () => {
+  it('Changes values when the slider slides', () => {
+    const inst = renderer.create(
+      <SliderQuestion
+        question="On a scale of 1 to 10 how much do you love Professor Duvall?"
+        min={0}
+        max={20}
+        step={1}
+        minLabel="He's the worst"
+        maxLabel="I love him he's the best"
+      />
+    );
+    const slider = inst.root.findByType(Slider);
+
+    expect(inst.root.instance.state.answer).toBe(10);
+    slider.props.onValueChange(2);
+    expect(inst.root.instance.state.answer).toBe(2);
   });
 });

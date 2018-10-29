1accfa074b66a53a59978e581402525b24962ad7
Nikolas Bramblett <nlb27@duke.edu>
2018-10-29 00:10:49 -0400

    Added actual test case! #61

--- docscribe/unit-testing/basic-test.js
+++ docscribe/unit-testing/basic-test.js
@@ -1,21 +1,42 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { CheckBox } from 'react-native-elements';
 import BinaryQuestion from '../src/components/BinaryQuestion';
 
 // Basic test made! (Try changing option two to "maybe" and it will fail!)
 // Snapshots basically take a "picture" of your code (serialized I think) and then in future tests
 // You can compare the component to the original snapshot. "npm test -- -u" will overwrite the snapshot for a test.
-it('renders correctly', () => {
-  const pain = renderer
-    .create(
+describe('<BinaryQuestion/>', () => {
+  it('renders correctly', () => {
+    const pain = renderer
+      .create(
+        <BinaryQuestion
+          question="Did you adhere to the prescribed usage guidelines today?"
+          optionOne="Yes"
+          optionTwo="No"
+          style={{ marginBottom: '20%' }}
+        />
+      )
+      .toJSON();
+    expect(pain).toMatchSnapshot();
+  });
+
+  it('Modifies values upon selection', () => {
+    const inst = renderer.create(
       <BinaryQuestion
         question="Did you adhere to the prescribed usage guidelines today?"
         optionOne="Yes"
         optionTwo="No"
         style={{ marginBottom: '20%' }}
       />
-    )
-    .toJSON();
-  expect(pain).toMatchSnapshot();
+    );
+    const checkBoxes = inst.root.findAllByType(CheckBox);
+
+    for (const box of checkBoxes) {
+      const one = inst.root.instance.state.oneChecked;
+      box.props.onPress();
+      expect(inst.root.instance.state.oneChecked).not.toBe(one);
+    }
+  });
 });

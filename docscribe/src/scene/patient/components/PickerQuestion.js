970135af2258909508fb2845a2ea51a389bed3e0
Ben Hubsch <bah37@duke.edu>
2018-11-08 14:45:58 -0500

    Handled lints with PickerQuestion.

--- docscribe/src/scene/patient/components/PickerQuestion.js
+++ docscribe/src/scene/patient/components/PickerQuestion.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Picker, Icon } from 'native-base';
 import PropTypes from 'prop-types';
+import AppStore from '../../../stores/AppStore';
+
+const DEFAULT = '0';
 
 export default class PickerQuestion extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chosen: '0',
       numbers: ['0', '1', '2', '3', '4', '5+'],
     };
   }
 
   render() {
     const { question, app, fb } = this.props;
-    const { numbers, chosen } = this.state;
+    const { numbers } = this.state;
     return (
       <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
         <Text>{question}</Text>
@@ -22,9 +24,8 @@ export default class PickerQuestion extends Component {
           mode="dropdown"
           iosIcon={<Icon name="ios-arrow-down-outline" />}
           style={{ width: undefined }}
-          selectedValue={app.object[fb]}
+          selectedValue={app.object[fb] ? app.object[fb] : DEFAULT}
           onValueChange={value => {
-            this.setState({ chosen: value });
             app.updateFirebase(fb, value);
             app.object[fb] = value;
           }}
@@ -40,4 +41,6 @@ export default class PickerQuestion extends Component {
 
 PickerQuestion.propTypes = {
   question: PropTypes.string.isRequired,
+  fb: PropTypes.string.isRequired,
+  app: PropTypes.instanceOf(AppStore).isRequired,
 };
